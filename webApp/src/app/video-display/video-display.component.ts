cf882cda4249c6500d87538f52cfb15c68f9eb5e
CamilleLeRoux <camille.le-roux1@etu.unice.fr>
2019-11-27 14:16:10 +0100

    #17 : nouvelle maniere de display la video

--- webApp/src/app/video-display/video-display.component.ts
+++ webApp/src/app/video-display/video-display.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import RxPlayer from "rx-player";
+import * as io from "socket.io-client";
 
 @Component({
   selector: 'app-video-display',
@@ -7,11 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoDisplayComponent implements OnInit {
 
-  constructor() { }
+  private url = "http://192.168.43.200:3000";
+  private socket;
+
+  constructor() {
+    this.socket = io(this.url);
+    console.log("Test");
+    this.socket.on('image', (image) => {
+      const imageElm = document.getElementById('image');
+      imageElm.setAttribute("src",`data:image/jpeg;base64,${image}`);
+      document.getElementById("loading").style.display = 'none';
+    });
+
+    this.socket.on("connect_error", function(exeception){
+      document.getElementById('image').setAttribute("src", "https://image.freepik.com/vecteurs-libre/modele-erreur-404-oiseau-dans-style-dessine-main_23-2147734776.jpg");
+      document.getElementById("loading").style.display = 'none';
+    })
+  }
 
   ngOnInit() {
 
-        var jsmpeg = require('jsmpeg');
+
+    /*var jsmpeg = require('jsmpeg');
         var canvas = document.createElement("canvas");
         document.body.appendChild(canvas);
         var client = new WebSocket('ws://192.168.43.223:8080/video-stream');
@@ -24,5 +43,15 @@ export class VideoDisplayComponent implements OnInit {
         x.onload = function(event){
           document.getElementById("loading").style.display = 'none';
         };
+
+        /*const player = new RxPlayer({
+          videoElement: document.getElementById("video")
+        });
+
+        player.loadVideo({
+          url : "http://192.168.43.14/video0.mpd",
+          transport: "dash",
+          autoplay: true
+        })*/
   }
 }
