0134553b1d88eb824073ca27d1d1faed8d3e4894
DUPONT Paul <paul.dupont@etu.unice.fr>
2019-12-02 14:54:24 +0100

    #25 : fix bug un server

--- localServer/controllers/foodControl.js
+++ localServer/controllers/foodControl.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const server = require('../server')
 var nodemailer = require('nodemailer');
-const socket = io.connect('http://192.168.43.77:3000');
+// const socket = server.io.connect('http://192.168.43.77:3000');
 
 const path = require('path');
 const { spawn } = require('child_process');
@@ -19,9 +19,10 @@ function runScript(image) {
 // La requéte contient le channel image de la socket
 async function setValue(req, res) {
     console.log(req.body.food)
-    var file = require('./ressources/ressources.json');
+    var file = require('./../ressources/ressources.json');
 
-    socket.emit('foodVideoStart', iterations);
+    // socket.emit('foodVideoStart', iterations);
+    server.io.emit('foodVideoStart', iterations);
 
     var score = 0
     nb_of_frames = 0
@@ -30,8 +31,10 @@ async function setValue(req, res) {
         nb_of_frames += 1 
     });
     
-    socket.emit('foodVideoStop');
+    // socket.emit('foodVideoStop');
+    server.io.emit('foodVideoStop');
     score = score / nb_of_frames
+    console.log(score);
 
     if (score > 0.2) {
         file.food = true
