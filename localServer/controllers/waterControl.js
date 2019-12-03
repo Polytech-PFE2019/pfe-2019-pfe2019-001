0e931c2653ee77a4af54c81871f76f447868a26f
DUPONT Paul <paul.dupont@etu.unice.fr>
2019-12-03 09:41:08 +0100

    #25 implement update etalon on the server

--- localServer/controllers/waterControl.js
+++ localServer/controllers/waterControl.js
@@ -27,7 +27,7 @@ async function setValue(req, res) {
     };
 
     console.log("Test : " + req.body.water);
-    var file = require('./../ressources.json');
+    var file = require('./../ressources/ressources.json');
     file.water = req.body.water;
     await fs.writeFileSync('ressources.json', JSON.stringify(file));
     server.io.emit('water', req.body.water);
