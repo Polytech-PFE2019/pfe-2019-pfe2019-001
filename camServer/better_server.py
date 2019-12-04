5929542d32010e500eed496865ceee64bc1bee26
Quentin <quentin.moreau@etu.unice.fr>
2019-12-04 10:21:07 +0100

    #21: Ajout de callback sur la demande de photo et possibilité de renseigner la qualité d'encodage

--- camServer/better_server.py
+++ camServer/better_server.py
@@ -5,22 +5,29 @@ import cv2
 import base64
 import subprocess
 
+#Récupération du lien vers la picam
 picam_proc = subprocess.Popen(["./picam.sh"], stdout=subprocess.PIPE)
 picam = picam_proc.stdout.read().decode("utf-8").strip().split()
 if len(picam) > 0:
     print("picam : " + " ".join(picam))
 
+#Récupération des liens vers les caméras usb
 usbcam_proc = subprocess.Popen(["./usbcam.sh"], stdout=subprocess.PIPE)
 usbcam = usbcam_proc.stdout.read().decode("utf8").strip().split()
 if len(usbcam) > 0:
     print("usbcams : " + " ".join(usbcam))
 
-encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 20]
 # Permet de patch les threads d'eventlet
 eventlet.monkey_patch()
+
+#Fusion des caméras en un tableau unique
 all_cameras = picam + usbcam
 camera_captures = []
 for camera in all_cameras:
+    # 1: Nom de la caméra
+    # 2: VideoCapture de la caméra
+    # 3: Liste des clients connectés au flux
+    # 4: Référence vers le thread de streaming
     camera_captures.append((camera, None, [], None))
 
 fps = 10
@@ -63,12 +70,19 @@ class setInterval :
 # 0 : channel
 # 1 : camera source
 # 2 : room
+# 3 : Encoding quality
 def sendImage(params):
     flag, frame = params[1].read()
     if flag:
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 20]
+        if not isinstance(params[3], bool) and isinstance(params[3], int) and params[3] >= 0 and params[3] <= 100:
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), params[3]]
         image = cv2.imencode('.jpg', frame, encode_param)[1].tostring();
         image = base64.b64encode(image)
-        sio.emit(params[0], image.decode('utf-8'), params[2]);
+        image = image.decode('utf-8')
+        sio.emit(params[0], image, params[2]);
+        return image
+    return False
 
 def bind_client(sid, camera_id):
     global camera_captures
@@ -99,7 +113,7 @@ def start_camera(id):
     global camera_captures
     temp = camera_captures[id]
     temp2 = cv2.VideoCapture(temp[0])
-    params = ["image", temp2, "camera_" + str(id)]
+    params = ["image", temp2, "camera_" + str(id), False]
     camera_captures[id] = (temp[0], temp2, temp[2], setInterval(1/float(fps), sendImage, params))
     print('## LOG ## Live started for camera ' + str(id))
 
@@ -122,6 +136,16 @@ def connect(sid, environ):
     bind_client(sid, 0)
 
 @sio.event
+def disconnect(sid):
+    print('## LOG ## Client disconnected, sid: ', str(sid))
+    remove_client(sid)
+
+@sio.event
+def camera_count(sid, data):
+    print("## LOG ## Camera count request")
+    return len(camera_captures)
+
+@sio.event
 def switch(sid, camera_id):
     if camera_id < len(camera_captures):
         print("## LOG ## Switching camera")
@@ -141,13 +165,9 @@ def picture(sid, data):
             id = i
             break
     if cap != None:
-        params = ["picture", cap, sid]
-        sendImage(params)
-
-@sio.event
-def disconnect(sid):
-    print('## LOG ## Client disconnected, sid: ', str(sid))
-    remove_client(sid)
+        params = ["picture", cap, sid, data]
+        return sendImage(params)
+    return False
 
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('', 3000)), app)
