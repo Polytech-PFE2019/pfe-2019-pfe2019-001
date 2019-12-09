9fa3bcaa6ab2490e233aee0ba04eda0e1451aee6
CamilleLeRoux <camille.le-roux1@etu.unice.fr>
2019-12-09 14:04:38 +0100

    #7 : ajout de la stat de nombre d'oiseau par jour

--- webApp/src/app/stats-display/stats-display.component.ts
+++ webApp/src/app/stats-display/stats-display.component.ts
@@ -18,46 +18,9 @@ export class StatsDisplayComponent implements OnInit {
   }
 
   ngOnInit() {
+    var waitUntil = require('wait-until');
     this.getWaterStats();
-    let dataPoints = [];
-    let dpsLength = 0;
-    let chart = new CanvasJS.Chart("chartContainer", {
-      exportEnabled: true,
-      title: {
-        text: "Live Chart with Data-Points from External JSON"
-      },
-      data: [{
-        type: "spline",
-        dataPoints: dataPoints,
-      }]
-    });
-
-    $.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=1&ystart=25&length=20&type=json&callback=?", function (data) {
-      $.each(data, function (key, value) {
-        dataPoints.push({ x: value[0], y: parseInt(value[1]) });
-      });
-      dpsLength = dataPoints.length;
-      chart.render();
-      updateChart();
-    });
-    function updateChart() {
-      $.getJSON("https://canvasjs.com/services/data/datapoints.php?xstart=" + (dpsLength + 1) + "&ystart=" + (dataPoints[dataPoints.length - 1].y) + "&length=1&type=json&callback=?", function (data) {
-        $.each(data, function (key, value) {
-          dataPoints.push({
-            x: parseInt(value[0]),
-            y: parseInt(value[1])
-          });
-          dpsLength++;
-        });
-
-        if (dataPoints.length > 20) {
-          dataPoints.shift();
-        }
-        chart.render();
-        setTimeout(function () { updateChart() }, 1000);
-      });
-    }
-
+    this.getCountStats();
   }
 
   getWaterStats() {
@@ -68,11 +31,11 @@ export class StatsDisplayComponent implements OnInit {
     waterStatsRef.once('value', function (snap) {
       snap.forEach(function (childSnap) {
         if (temp == undefined) {
-          temp = new Date(childSnap.child("/time").val() * 1000).valueOf()
+          temp = new Date(childSnap.child("/time").val()).valueOf()
         } else {
-          var time = new Date(childSnap.child("/time").val() * 1000).valueOf();
+          var time = new Date(childSnap.child("/time").val()).valueOf();
           const diffTime = Math.abs(time - temp);
-          const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+          const diffDays = Math.ceil(diffTime / (60 * 60 * 24));
           statsWater.push(diffDays);
           temp = time
         }
@@ -91,6 +54,42 @@ export class StatsDisplayComponent implements OnInit {
       var hours = (days - rdays) * 24;
       var rhours = Math.round(hours);
       document.getElementById("waterStat").innerHTML = rdays + " jour(s), " + rhours + " heure(s) et " + rminutes + " minute(s)";
+    });
+  }
+
+  getCountStats(){
+    var dataPoints = [];
+    var countRef = this.database.ref('/stats/birds_count');
+    countRef.once('value', function (snap) {
+      snap.forEach(function (childSnap) {
+        var obj = {y : childSnap.child("/value").val(), label: convertToDateFormat(new Date(childSnap.child("/time").val()))};
+        dataPoints.push(obj);
+      });
+      console.log(dataPoints);
+      let chart = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        exportEnabled: true,
+        title: {
+          text: "Nombre d'oiseaux par jour"
+        },
+        data: [{
+          type: "column",
+          indexLabel: "{y} oiseaux",
+          dataPoints: dataPoints/*[
+            { y: 71, label: "Apple" },
+            { y: 55, label: "Mango" },
+            { y: 50, label: "Orange" },
+            { y: 65, label: "Banana" },
+            { y: 95, label: "Pineapple" },
+            { y: 68, label: "Pears" },
+            { y: 28, label: "Grapes" },
+            { y: 34, label: "Lychee" },
+            { y: 14, label: "Jackfruit" }
+          ]*/
+        }]
+
+    });
+    chart.render();
     });
   }
 }
