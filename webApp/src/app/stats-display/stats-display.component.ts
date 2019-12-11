0cf2a5678b9e821b6dd8322e0c26ed5a07ef8b79
CamilleLeRoux <camille.le-roux1@etu.unice.fr>
2019-12-11 09:33:40 +0100

    #7 : Fix graph avec plusieurs dates

--- webApp/src/app/stats-display/stats-display.component.ts
+++ webApp/src/app/stats-display/stats-display.component.ts
@@ -73,20 +73,16 @@ export class StatsDisplayComponent implements OnInit {
           for (var i = 0; i < dataPoints.length; i++){
             console.log(dataPoints.length)
             var check = false;
-            console.log("Datapoint : " + dataPoints[i].label);
-            console.log("DateBdd : " + convertToDateFormat(new Date(childSnap.child("/time").val())))
             if (dataPoints[i].label == convertToDateFormat(new Date(childSnap.child("/time").val()))){
-              console.log("check !")
               dataPoints[i].y += childSnap.child("/value").val();
               check = true;
               break;
             }
-            if(!check){
-              console.log("pas check !")
-              var obj = {y : childSnap.child("/value").val(), label: convertToDateFormat(new Date(childSnap.child("/time").val()))};
-              dataPoints.push(obj);
-              check = false;
-            }
+          }
+          if(!check){
+            var obj = {y : childSnap.child("/value").val(), label: convertToDateFormat(new Date(childSnap.child("/time").val()))};
+            dataPoints.push(obj);
+            check = false;
           }
         }
       });
